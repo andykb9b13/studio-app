ff2cd7ce3789b2835b3a6ddbfa2844a01cf0d091
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-05-30 21:08:54 -0400

    created resolver for addStudent and querying students

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,41 @@
+const { Student } = require("../models");
+
+const resolvers = {
+  Query: {
+    students: async () => {
+      return await Student.find({});
+    },
+  },
+
+  Mutation: {
+    addStudent: async (
+      parent,
+      {
+        firstName,
+        lastName,
+        email,
+        primaryContact,
+        primaryContactEmail,
+        instrument,
+        school,
+        lessonLocation,
+        isActive,
+      }
+    ) => {
+      return await Student.create({
+        firstName,
+        lastName,
+        email,
+        primaryContact,
+        primaryContactEmail,
+        instrument,
+        lessonDay,
+        lessonTime,
+        grade,
+        school,
+        lessonLocation,
+        isActive,
+      });
+    },
+  },
+};
