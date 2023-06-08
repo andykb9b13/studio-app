d73b7f9f884bdd5f70e7ef1027928ee19b4408d6
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-06-08 11:12:46 -0400

    added imports for pages in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,24 @@ import {
 import { setContext } from "@apollo/client/link/context";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import addStudent from "./pages/addStudent";
+import assignmentView from "./pages/assignmentView";
+import createAssignment from "./pages/createAssignment";
+import landingPage from "./pages/landingPage";
+import loginPage from "./pages/loginPage";
+import practiceHub from "./pages/practiceHub";
+import signUpPage from "./pages/signUpPage";
+import streakPractice from "./pages/streakPractice";
+import studentDashboard from "./pages/studentDashboard";
+import studentDetails from "./pages/studentDetails";
+import teacherDashboard from "./pages/teacherDashboard";
+import timedPractice from "./pages/timedPractice";
+import weeklyPlan from "./pages/weeklyPlan";
+import tutorHome from "./pages/virtualTutor/tutorHome";
+import auralHome from ".pages/virtualTutor/auralHome";
+import conceptualHome from ".pages/virtualTutor/conceptualHome";
+import physicalHome from ".pages/virtualTutor/physicalHome";
+import visualHome from ".pages/virtualTutor/visualHome";
 
 const httpLink = createHttpLink({
   uri: "/graphql",
