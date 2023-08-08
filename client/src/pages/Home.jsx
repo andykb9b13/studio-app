3d9a3b15b4dad765a82bf8719d2237892b28c40c
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-08-08 14:08:39 -0400

    added basic scroll animation to homepage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,6 +12,7 @@ import {
   List,
   ListItem,
 } from "@mui/joy";
+import Animate from "../utils/ScrollAnimation";
 
 const styles = {
   card: {
@@ -32,116 +33,128 @@ const WelcomeCard = () => {
     alert("You are successfully logged out");
   };
   return (
-    <Card variant="outlined" sx={styles.card}>
-      <CardContent>
-        <Typography level="h1" component="h1">
-          Studio Hub
-        </Typography>
-        <Typography level="h2" component="h2">
-          Welcome!
-        </Typography>
-        <Typography level="h6" component="p">
-          Welcome to the Studio Management App. The one place where you can
-          manage all of your music studio needs so that your lessons can be more
-          efficient, productive, and versitile.
-        </Typography>
-        <List sx={styles.list}>
-          <ListItem>Create your own student databse</ListItem>
-          <ListItem>Create assignment plans for you students</ListItem>
-          <ListItem>Track your students' progress</ListItem>
-          <ListItem>
-            Use the Troubleshooting hub to help students identify what needs
-            work
-          </ListItem>
-          <ListItem>Access external resources for students</ListItem>
-        </List>
-      </CardContent>
-      <CardActions>
-        <Link to="/login">
-          <Button>Login</Button>
-        </Link>
-        {Auth.loggedIn() && (
-          <Box>
-            <Button onClick={logout}>Logout</Button>
-            <Button>Go To Profile</Button>
-          </Box>
-        )}
-        <Link to="/signup">
-          <Button>Teacher Sign Up</Button>
-        </Link>
-      </CardActions>
-    </Card>
+    <Animate anim="fade-right" height="100%">
+      <Card variant="outlined" sx={styles.card}>
+        <CardContent>
+          <Typography level="h1" component="h1">
+            Studio Hub
+          </Typography>
+          <Typography level="h2" component="h2">
+            Welcome!
+          </Typography>
+          <Typography level="h6" component="p">
+            Welcome to the Studio Management App. The one place where you can
+            manage all of your music studio needs so that your lessons can be
+            more efficient, productive, and versitile.
+          </Typography>
+          <List sx={styles.list}>
+            <ListItem>Create your own student databse</ListItem>
+            <ListItem>Create assignment plans for you students</ListItem>
+            <ListItem>Track your students' progress</ListItem>
+            <ListItem>
+              Use the Troubleshooting hub to help students identify what needs
+              work
+            </ListItem>
+            <ListItem>Access external resources for students</ListItem>
+          </List>
+        </CardContent>
+        <CardActions>
+          <Link to="/login">
+            <Button>Login</Button>
+          </Link>
+          {Auth.loggedIn() && (
+            <Box>
+              <Button onClick={logout}>Logout</Button>
+              <Button>Go To Profile</Button>
+            </Box>
+          )}
+          <Link to="/signup">
+            <Button>Teacher Sign Up</Button>
+          </Link>
+        </CardActions>
+      </Card>
+    </Animate>
   );
 };
 
 const AboutCard = () => {
   return (
-    <Card variant="outlined" sx={styles.card}>
-      <Typography level="h2">Get Organized...</Typography>
-      <Typography level="body1">
-        It is tough to stay on top of students, assignments, invoices, and more
-        when you are running a private teaching studio. This app is all about
-        helping you manage it easier!
-      </Typography>
-    </Card>
+    <Animate anim="fade-left" height="100%">
+      <Card variant="outlined" sx={styles.card}>
+        <Typography level="h2">Get Organized...</Typography>
+        <Typography level="body1">
+          It is tough to stay on top of students, assignments, invoices, and
+          more when you are running a private teaching studio. This app is all
+          about helping you manage it easier!
+        </Typography>
+      </Card>
+    </Animate>
   );
 };
 
 const AboutDatabaseCard = () => {
   return (
-    <Card variant="outlined" sx={styles.card}>
-      <Typography level="h2">
-        Keep Track of Your Database of Students
-      </Typography>
-      <List sx={styles.list}>
-        <ListItem>Search for students by age, instrument, or name</ListItem>
-        <ListItem>View Weekly Practice Plans</ListItem>
-        <ListItem>View emails</ListItem>
-        <ListItem>Edit student information</ListItem>
-      </List>
-    </Card>
+    <Animate anim="fade-left" height="100%">
+      <Card variant="outlined" sx={styles.card}>
+        <Typography level="h2">
+          Keep Track of Your Database of Students
+        </Typography>
+        <List sx={styles.list}>
+          <ListItem>Search for students by age, instrument, or name</ListItem>
+          <ListItem>View Weekly Practice Plans</ListItem>
+          <ListItem>View emails</ListItem>
+          <ListItem>Edit student information</ListItem>
+        </List>
+      </Card>
+    </Animate>
   );
 };
 
 const AboutSkillSheetCard = () => {
   return (
-    <Card variant="outlined" sx={styles.card}>
-      <Typography level="h2">Skill Sheets to test abilities</Typography>
-      <List sx={styles.list}>
-        <ListItem>Create skill sheets to challenge your students</ListItem>
-        <ListItem>Earn Badges for completing sheets</ListItem>
-      </List>
-    </Card>
+    <Animate anim="fade-right" height="100%">
+      <Card variant="outlined" sx={styles.card}>
+        <Typography level="h2">Skill Sheets to test abilities</Typography>
+        <List sx={styles.list}>
+          <ListItem>Create skill sheets to challenge your students</ListItem>
+          <ListItem>Earn Badges for completing sheets</ListItem>
+        </List>
+      </Card>
+    </Animate>
   );
 };
 
 const AboutPracticeHubCard = () => {
   return (
-    <Card variant="outlined" sx={styles.card}>
-      <Typography level="h2">Practice Hub for all your needs</Typography>
-      <List sx={styles.list}>
-        <ListItem>Timed Practicing</ListItem>
-        <ListItem>Go on a streak with a certain skill</ListItem>
-        <ListItem>View practice plans</ListItem>
-        <ListItem>Track your progress</ListItem>
-      </List>
-    </Card>
+    <Animate anim="fade-left" height="100%">
+      <Card variant="outlined" sx={styles.card}>
+        <Typography level="h2">Practice Hub for all your needs</Typography>
+        <List sx={styles.list}>
+          <ListItem>Timed Practicing</ListItem>
+          <ListItem>Go on a streak with a certain skill</ListItem>
+          <ListItem>View practice plans</ListItem>
+          <ListItem>Track your progress</ListItem>
+        </List>
+      </Card>
+    </Animate>
   );
 };
 
 const AboutVirtualTutorCard = () => {
   return (
-    <Card variant="outlined" sx={styles.card}>
-      <Typography level="h2">
-        Having Trouble?...The virtual tutor is here!
-      </Typography>
-      <List sx={styles.list}>
-        <ListItem>Get Help identifying the issue</ListItem>
-        <ListItem>
-          Step by stp instructons for troubleshooting the problem
-        </ListItem>
-      </List>
-    </Card>
+    <Animate anim="fade-right" height="100%">
+      <Card variant="outlined" sx={styles.card}>
+        <Typography level="h2">
+          Having Trouble?...The virtual tutor is here!
+        </Typography>
+        <List sx={styles.list}>
+          <ListItem>Get Help identifying the issue</ListItem>
+          <ListItem>
+            Step by stp instructons for troubleshooting the problem
+          </ListItem>
+        </List>
+      </Card>
+    </Animate>
   );
 };
 
---
+++ client/src/utils/ScrollAnimation.jsx
@@ -0,0 +1,22 @@
+import { Box } from "@mui/material";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
+
+const Animate = ({
+  children,
+  anim = "zoom-in-up",
+  delay = "0",
+  height = "auto",
+}) => {
+  useEffect(() => {
+    AOS.init({ duration: 1000, once: true });
+  });
+  return (
+    <Box sx={{ height: { height } }} data-aos={anim} data-aos-delay={delay}>
+      {children}
+    </Box>
+  );
+};
+
+export default Animate;
