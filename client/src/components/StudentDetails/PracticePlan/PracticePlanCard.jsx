63ae81e6699e092771e14ff161182cb51877efd7
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-08-17 11:28:46 -0400

    changed names of components for PracticePlan and lifted the state up so the page can rerender when a new plan is created

--- client/src/components/StudentDetails/PracticePlan/PracticePlanCard.jsx
+++ client/src/components/StudentDetails/PracticePlan/PracticePlanCard.jsx
@@ -1,33 +1,93 @@
-import React, { useState, useContext } from "react";
-import CreatePracticePlan from "./CreatePracticePlan";
-import PracticePlan from "./PracticePlan";
-import { Typography, Button } from "@mui/joy";
-import { StudentContext } from "../../../pages/StudentDetails";
+import React, { useState } from "react";
+import CreateAssignment from "./Assignments/CreateAssignment";
+import { Sheet, Typography, IconButton, Table } from "@mui/joy";
+import { Add } from "@mui/icons-material";
+import AssignmentView from "./Assignments/AssignmentView";
 import RegularModal from "../../common/Modal/RegularModal";
+import DeleteModalContent from "../../common/Modal/DeleteModalContent";
+import { useMutation } from "@apollo/client";
+import { DELETE_PRACTICE_PLAN } from "../../../utils/mutations";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-// displays practice plans from a student
-export default function PracticePlanCard() {
-  const { id, practicePlans } = useContext(StudentContext);
+const styles = {
+  sheet: {
+    mt: 3,
+    p: 2,
+    backgroundColor: "lavender",
+    borderRadius: "4px",
+    boxShadow: "md",
+  },
+};
+
+// The view of an individual practice plan
+const PracticePlanCard = ({ practicePlan }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
   const [open, setOpen] = useState(false);
 
+  const [deletePracticePlan, { error }] = useMutation(DELETE_PRACTICE_PLAN);
+
+  console.log(practicePlan);
+
+  const deletePracticePlanFunc = async () => {
+    await deletePracticePlan({
+      variables: { planId: practicePlan._id },
+    });
+    alert("Plan Deleted!");
+    setOpen(false);
+  };
+
   return (
-    <React.Fragment>
-      <Typography level="h2">Practice Plans</Typography>
+    <Sheet sx={styles.sheet}>
+      <Typography level="h2">{practicePlan.name}</Typography>
+      <IconButton onClick={() => setActiveIndex(1)}>
+        <Add />
+      </IconButton>
+
       <RegularModal open={open} onRequestClose={() => setOpen(false)}>
-        <CreatePracticePlan
+        <DeleteModalContent
           onRequestClose={() => setOpen(false)}
-          resourceName="Create Practice Plan"
+          confirmAction={() => deletePracticePlanFunc()}
+          resourceName="Practice Plan"
         />
       </RegularModal>
-      <Button onClick={() => setOpen(true)}>Create Practice Plan</Button>
-      {practicePlans &&
-        practicePlans.map((practicePlan) => (
-          <PracticePlan
-            practicePlan={practicePlan}
-            studentId={id}
-            key={practicePlan._id}
-          />
-        ))}
-    </React.Fragment>
+      <IconButton onClick={() => setOpen(true)}>
+        <DeleteIcon />
+      </IconButton>
+
+      {activeIndex === 1 ? <CreateAssignment planId={practicePlan._id} /> : ""}
+
+      <Table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Metronome</th>
+            <th>Source</th>
+            <th>Details</th>
+          </tr>
+        </thead>
+        <tbody>
+          {practicePlan.assignments &&
+            practicePlan.assignments.map((assignment) => (
+              <React.Fragment key={assignment._id}>
+                <tr>
+                  <td>
+                    <b>{assignment.exerciseName}</b>
+                  </td>
+                  <td>{assignment.metronome}</td>
+                  <td>{assignment.source}</td>
+                  <td>
+                    <AssignmentView
+                      assignment={assignment}
+                      key={assignment._id}
+                    />
+                  </td>
+                </tr>
+              </React.Fragment>
+            ))}
+        </tbody>
+      </Table>
+    </Sheet>
   );
-}
+};
+
+export default PracticePlanCard;
