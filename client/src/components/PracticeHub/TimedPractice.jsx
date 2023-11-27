2f855cd40c9e37ff903eb1f88066d3ce11755957
Andy Kleindienst <andy.kleindienst@gmail.com>
2023-11-26 22:23:41 -0500

    added functionality to the timer in practiceHub

--- client/src/components/PracticeHub/TimedPractice.jsx
+++ client/src/components/PracticeHub/TimedPractice.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Typography,
   Button,
@@ -11,6 +11,38 @@ import {
 } from "@mui/joy";
 
 const TimedPractice = ({ student }) => {
+  const [count, setCount] = useState(0);
+  const [timer, setTimer] = useState(null);
+
+  const startTimerFunc = () => {
+    setTimer(
+      setInterval(() => {
+        setCount((prevCount) => {
+          if (prevCount === 0) {
+            clearInterval(timer);
+            console.log("Time's up!");
+            return 0;
+          } else {
+            return prevCount - 1;
+          }
+        });
+      }, 1000)
+    );
+  };
+
+  const stopTimerFunc = () => {
+    clearInterval(timer);
+    setTimer(null);
+    setCount(0);
+  };
+
+  useEffect(() => {
+    return () => {
+      // Clear the interval when the component is unmounted
+      clearInterval(timer);
+    };
+  }, [timer]);
+
   return (
     <Grid container mt={4}>
       <Card variant="outlined" sx={{ mx: "auto" }}>
@@ -25,16 +57,24 @@ const TimedPractice = ({ student }) => {
             color="primary"
             sx={{ fontSize: "10em" }}
           >
-            60
+            {count}
           </Typography>
         </Box>
         <CardContent>
           <Typography level="h3">Set Time</Typography>
-          <Input type="number" placeholder="minutes" />
+          <Input
+            type="number"
+            placeholder="seconds"
+            onChange={(e) => setCount(parseInt(e.target.value, 10))}
+          />
         </CardContent>
         <CardActions sx={{ mx: "auto" }}>
-          <Button color="success">Start</Button>
-          <Button color="danger">Stop</Button>
+          <Button color="success" onClick={startTimerFunc}>
+            Start
+          </Button>
+          <Button color="danger" onClick={stopTimerFunc}>
+            Stop
+          </Button>
         </CardActions>
       </Card>
     </Grid>
